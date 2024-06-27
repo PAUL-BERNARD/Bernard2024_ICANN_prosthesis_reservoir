c8560e530555015d9fedb3e832f2ac629197fb2e
PAUL BERNARD <Paul.Bernard@inria.fr>
2024-06-27 14:31:22 +0200

    refact metrics + all_distances

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,18 +1,13 @@
 import numpy as np
 
+# Last step
+
 
 def last_step_indices(info):
     tgtNumber = info[:, 2]
 
     tgtNumberChange = tgtNumber[1:] != tgtNumber[:-1]
     return np.where(tgtNumberChange)[0]
-
-
-def red_indices(info):
-    tgtRed = info[:, 3]
-
-    red_indices = np.where(tgtRed)[0]
-    return red_indices
 
 
 def last_step_distances_nparray(x, y, info):
@@ -38,7 +33,17 @@ def last_step_distances(x, y, info):
         and isinstance(y, np.array)
         and isinstance(info, np.array)
     ):
-        return last_step_distances(x, y, info)
+        return last_step_distances_nparray(x, y, info)
+
+
+# Red steps
+
+
+def red_indices(info):
+    tgtRed = info[:, 3]
+
+    red_indices = np.where(tgtRed)[0]
+    return red_indices
 
 
 def red_distances_nparray(x, y, info):
@@ -63,4 +68,33 @@ def red_distances(x, y, info):
         and isinstance(y, np.array)
         and isinstance(info, np.array)
     ):
-        return red_distances(x, y, info)
+        return red_distances_nparray(x, y, info)
+
+
+# All distances
+
+
+def all_distances_nparray(x, test, pred):
+    hand = test
+    model = pred
+    target = x[:, 2:7]
+
+    hand_model = np.sqrt(np.mean(np.square(hand - model), axis=1))
+    target_model = np.sqrt(np.mean(np.square(target - model), axis=1))
+
+    return hand_model, target_model
+
+
+def all_distances(x, test, pred):
+    if isinstance(x, list) and isinstance(test, list) and isinstance(pred, list):
+        return [
+            all_distances_nparray(x_, test_, pred_)
+            for x_, test_, pred_ in zip(x, test, pred)
+        ]
+
+    if (
+        isinstance(x, np.array)
+        and isinstance(test, np.array)
+        and isinstance(pred, np.array)
+    ):
+        return all_distances(x, test, pred)
